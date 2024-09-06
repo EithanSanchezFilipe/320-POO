4f986fcfca9e972a9f4444009bbf91323e8c037a
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-09-06 09:09:26 +0200

    feat(Parachute): Ajout de la classe para

---
+++ personnel/ParaClub/ParaClub/Para.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaClub
+{
+    public class Para
+    {
+        public string Name;
+        public int _y;
+
+        public Para(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
--- personnel/ParaClub/ParaClub/Program.cs
+++ personnel/ParaClub/ParaClub/Program.cs
@@ -12,6 +12,7 @@ namespace ParaClub
         {
             Console.SetWindowSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
             Plane plane = new Plane();
+            Para bob = new Para("bob");
             
             while (plane._x < Config.SCREEN_WIDTH)
             {
