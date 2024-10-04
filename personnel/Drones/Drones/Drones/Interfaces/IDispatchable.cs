584cfedba842adb64f658f026dbb5d4443f0aa56
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-04 09:30:29 +0200

    feat(drone): nouvelle interface IDispatchable

---
+++ personnel/Drones/Drones/Drones/Interfaces/IDispatchable.cs
@@ -0,0 +1,16 @@
+﻿using Drones.Model;
+
+namespace Drones
+{
+    public interface IDispatchable
+    {
+        // Signal the limits of the no-fly zone 
+        // Return true if the drone is already outside the zone
+        List<Box> Boxes { get; set; }
+        // Terminate the no-fly zone
+        public void CreateBox(Box box);
+
+        // Interrogate the drone
+        public void RemoveBox(Box box);
+    }
+}
