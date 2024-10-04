7867ffb4871780f7329a6034f1ca36b1a0f32291
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-04 09:04:04 +0200

    ADD(POO): id factory

--- personnel/Drones/Drones/Drones/Model/Factory.cs
+++ personnel/Drones/Drones/Drones/Model/Factory.cs
@@ -3,9 +3,11 @@
     public partial class Factory : Building
     {
         private float PowerConsumption;
+        public static int id;
         public Factory(int posX, int posY, int Width, int Height, float PowerConsumption) : base(posX, posY, Width, Height)
         {
             this.PowerConsumption = PowerConsumption;
+            ++id;
             Show();
         }
         public void Show()
