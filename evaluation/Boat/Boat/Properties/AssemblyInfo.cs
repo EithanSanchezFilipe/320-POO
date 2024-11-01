3764b440087884fd497c7976e024ae60a8eb6384
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-11-01 09:25:21 +0100

    chore(Boat): Création du projet

---
+++ evaluation/Boat/Boat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Boat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DGEP")]
+[assembly: AssemblyProduct("Boat")]
+[assembly: AssemblyCopyright("Copyright © DGEP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("a3e28917-dd7b-4ff1-a706-98d16d542d2c")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ personnel/Boat/Boat/Boat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boat
+{
+    internal class Boat
+    {
+        private int _year;
+        private int _tonnage;
+        private string _country;
+
+
+        private static void Load()
+        {
+
+        }
+        private static void Unload() 
+        {
+
+        }
+    }
+}
